473a15a648daecd5293a067d67ea5a572165df65
Angelo <vageez@gmail.com>
2023-05-20 19:41:12 -0400

    Setting up new dir structure

---
+++ src/app/head.tsx
@@ -0,0 +1,11 @@
+
+export default function Head() {
+  return (
+    <>
+      <title>Gecko Gold Coin</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Gecko Gold Coin" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Gecko Gold Coin</h1>
+    </main >
+  )
+}
